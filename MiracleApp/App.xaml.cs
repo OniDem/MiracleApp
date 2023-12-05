0edc4dacac866dd7edb1678581dbbaa5e7f4fb7e
OniDem1 <spaindecraft17@gmail.com>
2023-12-05 20:47:29 +0700

    Добавьте файлы проекта.

---
+++ MiracleApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MiracleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
