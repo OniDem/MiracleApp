a5feadac99ee4a8c10c38c5542f1ace86c09e4a4
dima3998 <102709160+dima3998@users.noreply.github.com>
2023-12-11 14:50:31 +0700

    добавил entity

---
+++ MiracleApp/Entity/TitleNewsFrameEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiracleApp.Entity
+{
+    class TitleNewsFrameEntity
+    {
+        string newsName;
+        string newsImage;
+        string newsContent;
+    }
+}
