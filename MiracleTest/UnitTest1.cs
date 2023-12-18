84a2238665c9baadc3ab12eea82a25295ed8555a
OniDem1 <spaindecraft17@gmail.com>
2023-12-18 13:13:27 +0700

    Добавил проект с тестами

---
+++ MiracleTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MiracleTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
