1c53a6ddc96d4499a56e600fe5516188b743aa58
OniDem1 <spaindecraft17@gmail.com>
2024-01-04 13:58:32 +0700

    Добавил вывод текущего пользователя и кнопки для выхода из учётки, изменения профиля

--- MiracleApp/Pages/ProfilePage.xaml.cs
+++ MiracleApp/Pages/ProfilePage.xaml.cs
@@ -1,9 +1,13 @@
+using CommunityToolkit.Maui.Alerts;
+using Core.Entity;
+using MiracleApp.Services.User;
 using MiracleApp.Validation;
 
 namespace MiracleApp.Pages;
 
 public partial class ProfilePage : ContentPage
 {
+    public UserEntity user = new();
 
     public ProfilePage()
     {
@@ -11,11 +15,18 @@ public partial class ProfilePage : ContentPage
         {
             Navigation.PushAsync(new HelloPage());
         }
+        Dispatcher.Dispatch(async () =>
+        {
+            UserInfoVL.BindingContext = await UserService.GetUserById(new() { id = Convert.ToInt32(await SecureStorage.GetAsync("id")) });
+        });
         InitializeComponent();
-        SettingsButton.Source = "settings.png";
-        HomeButton.Source = "home.png";
-        ProfileButton.Source = "profile.png";
-        NotificationButton.Source = "notification.png";
+        SettingsButton.Source = "settings.svg";
+        HomeButton.Source = "home.svg";
+        ProfileButton.Source = "profile_selected.svg";
+        NotificationButton.Source = "notification.svg";
+        LogOut.Source = "logout.svg";
+        ProfileEdit.Source = "prodile_edit.svg";
+
     }
 
     private async void SettingsButton_Clicked(object sender, EventArgs e)
@@ -41,65 +52,12 @@ public partial class ProfilePage : ContentPage
     private void LogOut_Clicked(object sender, EventArgs e)
     {
         SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
-        SecureStorage.RemoveAll();
         Navigation.PushAsync(new HelloPage());
+    }
+
+    private void ProfileEdit_Clicked(object sender, EventArgs e)
+    {
+        var toast = Toast.Make("Данная кнопка в разработке!", CommunityToolkit.Maui.Core.ToastDuration.Long);
+        toast.Show();
     }
 }
