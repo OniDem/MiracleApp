737d02df42d25accb2b55a497d13906e985d4c1b
OniDem1 <spaindecraft17@gmail.com>
2024-01-20 17:36:03 +0700

    Добавил сущность отображения занятий при выводе(вынес со страницы)

---
+++ Core/Entity/ShowLessonEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entity
+{
+    public class ShowLessonEntity
+    {
+        public string Description { get; set; }
+
+        public string TimeStart { get; set; }
+
+        public string TimeEnd { get; set; }
+
+        public string Where { get; set; }
+    }
+}
