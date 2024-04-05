057cc3b84995626c2e42497976eb0168cb370859
OniDem1 <spaindecraft17@gmail.com>
2024-04-05 12:27:13 +0700

    *

--- MiracleApi/Program.cs
+++ MiracleApi/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opt =>
 {
-    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MiracleApi", Version = "Dev0.1" });
     opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
