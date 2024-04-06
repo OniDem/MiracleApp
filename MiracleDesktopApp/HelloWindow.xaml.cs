3d09d98e40f21cb1adc823386d4f4cfa74e9ea24
OniDem1 <Spaindecraft17@gmail.com>
2024-04-06 22:27:08 +0700

    Добавил в решение проект WPF(десктоп)

---
+++ MiracleDesktopApp/HelloWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MiracleDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for HelloWindow.xaml
+    /// </summary>
+    public partial class HelloWindow : Window
+    {
+        public HelloWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
